d9166f044d34701d82d68184590cef100091cd09
Md. Abu Sakib Chowdhury <sakib21eee@gmail.com>
2024-08-30 20:37:26 +0600

    task 4 completed

--- Task4.js
+++ Task4.js
@@ -1,3 +1,4 @@
+/*
 ### Task-4
 
 What will be the result of the following codes:
@@ -7,3 +8,8 @@ What will be the result of the following codes:
 **var a = isNaN(2-10);**
 
 Explain your answers.
+*/
+var a = isNaN('10');
+console.log(a);
+var b = isNaN(2-10);
+console.log(b);
